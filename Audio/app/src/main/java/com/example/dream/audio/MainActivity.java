3261b4afe4a43406b09f3f334b19a2146fde3c60
Learn French <mansi.jain.kota@gmail.com>
2017-02-12 15:45:48 +0530

    first commit

---
+++ Audio/app/src/main/java/com/example/dream/audio/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.dream.audio;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.tum_saath_ho);
+        Button b1 = (Button) findViewById(R.id.play_button);
+        Button b2 = (Button) findViewById(R.id.pause_button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+
+
+    }
+
+}
